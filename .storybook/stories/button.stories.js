cfa96766bff880bbe6dc4c653885d66215e45e68
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2019-06-07 23:00:50 -0300

    chore: Update local versions

--- .storybook/stories/button.stories.js
+++ .storybook/stories/button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { Button } from '@storybook/react/demo';
+
 import Button from '../../components/button/src'
 
 storiesOf('Button', module)
